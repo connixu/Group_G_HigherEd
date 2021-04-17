b63b944e6424d6622fc104eac0bdcdf62cff9983
aeherman <aeh2196@columbia.edu>
2021-04-17 17:38:14 -0400

    element_rect(fill='white')

--- src/visuals/ourtheme.R
+++ src/visuals/ourtheme.R
@@ -1,5 +1,5 @@
 ourtheme <- ggplot2::theme(text = element_text(family = "sansserif"),
-                  panel.background = element_blank(),
-                  legend.position = "bottom",
-                  axis.ticks = element_blank(),
-                  plot.title = element_text(hjust=0.5))
+                           panel.background= element_rect(fill="white"),
+                           legend.position = "bottom",
+                           axis.ticks = element_blank(),
+                           plot.title = element_text(hjust=0.5))
