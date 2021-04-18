82680dcc313d8d0b1188d6935e3eda1b6c538b03
aeherman <aeh2196@columbia.edu>
2021-04-18 17:14:56 -0400

    specify subtitle position

--- src/visuals/ourtheme.R
+++ src/visuals/ourtheme.R
@@ -3,4 +3,6 @@ ourtheme <- ggplot2::theme(text = element_text(family = "serif"),
                            panel.background = element_rect(fill = "white"),
                            legend.position = "bottom",
                            axis.ticks = element_blank(),
-                           plot.title = element_text(hjust = 0.5))
+                           plot.title = element_text(hjust = 0.5),
+                           plot.subtitle = element_text(hjust = 0.5),
+                           plot.caption = element_text(hjust = 0))
